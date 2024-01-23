fdce89d46687b137284eb5bad1c30d4e138a3c0d
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-01-23 19:08:06 -0300

    finalizando seção 19

--- seção_19/js/scripts.js
+++ seção_19/js/scripts.js
@@ -47,7 +47,7 @@ console.log(mySecondObject);
 console.log(Object.getPrototypeOf(mySecondObject) === myObj);
 
 //Classes básicas
-const dog = {
+const cachorro = {
     raca:null
 };
 
@@ -100,5 +100,103 @@ console.log(jeff);
 console.log(Object.getPrototypeOf(jeff));
 
 //Mais sobre classes
+class Caminhao {
+    constructor(eixos, cor) {
+        this.eixos = eixos;
+        this.cor = cor;
+    };
 
+    descreveCaminhao() {
+        console.log(
+            `Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}`
+        );
+    };
+};
+
+const scania = new Caminhao(6, "Vermelha");
+scania.descreveCaminhao();
+Caminhao.motor = 4.0;
+const c2 = new Caminhao(4, "Preta");
+
+//Motor é undefined
+console.log(c2.motor);
+Caminhao.prototype.motor = 4.0;
+const c3 = new Caminhao(6, "Azul");
+console.log(c3.motor);
+
+//override por prototype
+class Humano {
+    constructor(nome, idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+const lucas = new Humano("Lucas", 31);
+console.log(lucas);
+
+Humano.prototype.idade = "Não definida";
+
+console.log(lucas.idade);
+console.log(Humano.prototype.idade);
+
+//Symbol
+class Aviao {
+    constructor(marca, turbinas) {
+        this.marca = marca;
+        this.turbinas = turbinas;
+    }
+}
+
+const asas = Symbol();
+Aviao.prototype[asas] = 2;
+const boeing = new Aviao("Boeing", 10);
+console.log(boeing);
+console.log(boeing[asas]);
+console.log(Aviao.prototype[asas]);
+
+//Getter Setter
+class Post {
+    constructor(titulo, descricao, tags) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.tags = tags;
+    };
+
+    get exibirTitulo() {
+        return this.titulo;
+    };
+
+    set adicionarTag(tags) {
+        this.tags = tags;
+    };
+
+};
+
+const myPost = new Post("Texto do post", "É um post sobre programação");
+console.log(myPost.exibirTitulo);
+myPost.adicionarTag = ("programação, js");
+console.log(myPost)
+
+//Herança
+class Mamifero {
+    constructor(patas) {
+        this.patas = patas;
+    };
+};
+
+class Lobo extends Mamifero {
+    constructor(patas, nome) {
+        super(patas, patas);
+        this.nome = nome;
+    };
+};
+
+const shark = new Lobo(4, "Shark");
+
+console.log(shark);
+
+//InstanceOF
+console.log(shark instanceof Lobo);
+console.log(Lobo instanceof Mamifero);
 
