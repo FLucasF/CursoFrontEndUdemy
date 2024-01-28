a492cea27e3a50230a5c5a55d073e5ae850efeb7
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-01-28 17:59:31 -0300

    finalizando js moderno

--- seção_28/js/script.js
+++ seção_28/js/script.js
@@ -82,3 +82,101 @@ const users = [
 
 const availableUsers = users.filter((user) => user.available);
 console.log(availableUsers);
+
+//4 Map
+const products = [
+    {name: "Camisa", price: 10.99, category: "Roupa" },
+    {name: "Cheleira elétrica", price: 150 , category: "Eletro"},
+    {name: "Fogão", price: 499, category: "Eletro"},
+    {name: "Calça jeans", price: 49.99, category: "Roupa"},
+];
+
+products.map((products) => {
+    if(products.category === "Roupa") {
+        products.onSale = true;
+    }
+});
+
+console.log(products);
+
+//5 template literals
+const userName = "Matheus";
+const age = 30;
+
+console.log(`O nome do usuário é ${userName}  e ele tem ${age} anos!`);
+
+//6 destructuring
+const fruits = ["Maçã", "Mamão", "Laranja"];
+const [f1, f2, f3] = fruist;
+
+console.log(f1);
+
+const productDetails = {
+    name: productsName,
+    price: productPrice,
+    category: productCategory,
+    color,
+} = productDetails;
+
+console.log(
+    `O nome do produto é ${productName}, custa R$${producPrice} e é da cor ${color}`
+);
+
+//7 spread operator
+const a1 = [1,2,3];
+const a2 = [4,5,6];
+
+console.log(a3);
+
+const a4 = [0, ...a1, 4];
+
+console.log(a4);
+
+const carName = {name: "Gold"};
+const carBrand = {brand: "VW"};
+const otherInfos = {km: 100000, price: 49000};
+
+const car = {...carName, ...carBrand, ...otherInfos};
+
+console.log(car);
+
+//8 classes
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    productWithDiscount(discount) {
+        return this.price * ((100 - discount) / 100);
+    }
+}
+
+const shirt = new Product("Cmisa gola V", 20);
+
+console.log(shirt.productWithDiscount(10));
+
+//9 heranca
+class ProductWithAttributes extends Product {
+    constructor(name, price, colors) {
+        super(name, price);
+        this.colors = colors;
+    }
+
+    showColors() {
+        console.log("As cores são: ");
+        this.colors.forEach(color => {
+            console.log(color);            
+        });
+    }
+}
+
+const hat = new ProductWithAttributes("Chapéu", 29.99, [
+    "Preto",
+    "Amarelo",
+    "Verde"
+]);
+
+console.log(hat.name);
+
+hat.showColors();
