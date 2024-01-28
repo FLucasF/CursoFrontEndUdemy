78aace7a9c4f425207d36d833517715445d2c063
FLucasF <lucasfelipegpGITHUB@gmail.com>
2024-01-28 16:44:53 -0300

    js moderno

---
+++ seção_28/js/script.js
@@ -0,0 +1,84 @@
+//1 var, let e const
+var x = 10;
+var y = 15;
+
+if(y > 10) {
+    var x = 5;
+    console.log(x);
+};
+
+console.log(x);
+
+let a = 10;
+let b = 15;
+
+if(b > 10) {
+    let a = 5;
+    console.log(a);
+};
+
+console.log(a);
+
+function logName() {
+    const name = "Pedro";
+    console.log(name);
+};
+
+const name = "Matheus";
+logName();
+
+//2 arrow function
+const sum = function(a, b) {
+    return a + b;
+};
+
+const arrowSum = (a, b) => a + b;
+
+console.log(sum(5, 5));
+console.log(arrowSum(5, 5));
+
+const greeting = (name) => {
+    if(name) return "Hello " + name + "!";
+    else return "Hello!";
+};
+
+console.log(greeting());
+console.log(greeting("Lucas"));
+
+const user = {
+    name: "Theo",
+    sayUserName() {
+        setTimeout(function() {
+            console.log(this);
+            console.log("Username: " + this.name);
+        }, 1000);
+    },
+    sayUserNameArrow() {
+        setTimeout(() => {
+            console.log(this);
+            console.log("Username: " + this.name);
+        });
+    }
+};
+
+//user.sayUserName();
+//user.sayUserNameArrow();
+
+//3 filter
+const arr = [1,2,3,4,5];
+
+const highNumbers = arr.filter((n) => {
+    if(n >= 3) return n;
+});
+
+console.log(highNumbers);
+
+const users = [
+    {name: "Matheus", available: true},
+    {name: "Pedroa", available: false},
+    {name: "Marco", available: true},
+    {name: "Josias", available: false},
+];
+
+const availableUsers = users.filter((user) => user.available);
+console.log(availableUsers);
